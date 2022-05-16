0686ac5c5d73b4cf64cb501add9aa73221c7e6d1
marckraw <marckraw@icloud.com>
2022-05-16 20:26:40 +0200

    fix: downgrade @types/react - storyblok-react package is not ready for it

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 
-interface LayoutProps {}
+interface LayoutProps {
+  children: React.ReactNode;
+}
 
 const Layout: React.FC<LayoutProps> = (props) => {
   const { children } = props;
